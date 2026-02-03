0e8a94d739dec8e96c202b573af2329d58afe6b4
22sahib22 <ssing791@mtroyal.ca>
2026-02-03 16:22:56 -0700

    pdf parser, however its a little broken and places all dates in one calender event.

---
+++ parseLogic.py
@@ -0,0 +1,80 @@
+import re
+from pypdf import PdfReader
+
+
+# event class 
+class calendarEvent:
+    def __init__(self, title, date, source_line):
+        self.title = title
+        self.date = date
+        self.source_line = source_line
+
+    def __repr__(self):
+        return f"Event(title={self.title}, date={self.date})"
+
+
+#keywords and date patters
+
+keywords = ["assignment", "quiz", "test", "exam", "lab", "tutorial", "project"]
+
+DATE_PATTERN = re.compile(
+    r"(\b\d{4}-\d{2}-\d{2}\b|"          # the format 2026-03-15
+    r"\b(?:jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)[a-z]*\s+\d{1,2}"
+    r"(?:,\s*\d{4})?\b)",
+    re.IGNORECASE
+)
+
+#helper to find keywords
+def find_keyword(line):
+    for kw in keywords:
+        if kw in line:
+            return kw
+    return None
+
+#parse pdf
+
+def parsePDF(file_path):
+    events = []
+
+    reader = PdfReader(file_path)
+
+    for page in reader.pages:
+        text = page.extract_text()
+        if not text:
+            continue
+
+        lines = text.split("\n")
+        for line in lines:
+            lower_line = line.lower()
+
+            #check keyword
+            keyword = find_keyword(lower_line)
+            if not keyword:
+                continue
+
+            #check date
+            date_match = DATE_PATTERN.search(line)
+            if not date_match:
+                continue
+
+            date_str = date_match.group()
+
+            #prepare event for api
+            event = calendarEvent(
+                title=keyword.capitalize(),
+                date=date_str,
+                source_line = line.strip()
+            )
+
+            events.append(event)
+
+    return events
+
+#test
+if __name__ == "__main__":
+    events = parsePDF("example1.pdf")
+
+    for event in events:
+        print(event)
+        print("Source: ", event.source_line)
+        print()
